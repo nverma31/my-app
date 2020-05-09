9e0d4886723ad17731c1ec32af1d233013784ef8
Neeraj Verma <nverma31@gmail.com>
2020-05-09 11:46:31 +0200

    Updates

---
+++ precache-manifest.05079939710d95d62edaa02e4a1bf79e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0354aa9aac939b80ed6a89d5a01220a2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4fb9192705ff9bd667b7",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/my-app/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb9192705ff9bd667b7",
+    "url": "/my-app/static/js/main.aebec170.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
