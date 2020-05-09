22752e970683bb78a81efbb99f4142d5345b9805
Neeraj Verma <nverma31@gmail.com>
2020-05-09 11:56:57 +0200

    Updates

---
+++ precache-manifest.c9a7a1d8bccd88e85b283945fa7676a2.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "a60f2de1db4967b5bf59",
+    "url": "/my-app/static/css/main.5261dbc0.chunk.css"
+  },
+  {
+    "revision": "a60f2de1db4967b5bf59",
+    "url": "/my-app/static/js/main.a987b727.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "89b45c57767f289fa2c9",
+    "url": "/my-app/static/js/2.62a0a02f.chunk.js"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/my-app/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "2153a12d1db4eda5b954e54532175e5d",
+    "url": "/my-app/static/media/CircularStd-BlackItalic.2153a12d.woff"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/my-app/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "8d5dbe8149820b1202cbaf34b7204598",
+    "url": "/my-app/static/media/CircularStd-BoldItalic.8d5dbe81.woff"
+  },
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/my-app/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "eddb70218f38749eaeeed7fba894fda8",
+    "url": "/my-app/static/media/CircularStd-MediumItalic.eddb7021.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/my-app/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "cc06409e5a820f455b3b4883fbf9b72f",
+    "url": "/my-app/static/media/CircularStd-BookItalic.cc06409e.woff"
+  },
+  {
+    "revision": "8a448bc66237a93eacd189dc4c47d2a2",
+    "url": "/my-app/static/media/project-tn-01.8a448bc6.png"
+  },
+  {
+    "revision": "d283abb90c6642e90f21a0c1255abe50",
+    "url": "/my-app/static/media/project-tn-02.d283abb9.png"
+  },
+  {
+    "revision": "bc5e09384b85dc241c545a7c3217b90c",
+    "url": "/my-app/static/media/project-tn-03.bc5e0938.png"
+  },
+  {
+    "revision": "d7243ea0712f9b0875e97cd539ad0ed4",
+    "url": "/my-app/static/media/project-tn-04.d7243ea0.png"
+  },
+  {
+    "revision": "db8c6505e94b4da1b477257ad781899a",
+    "url": "/my-app/static/media/where-img00.db8c6505.png"
+  },
+  {
+    "revision": "1b6e4242f35daef4f52d40fa21cd8dba",
+    "url": "/my-app/static/media/where-img01.1b6e4242.png"
+  },
+  {
+    "revision": "b02b1c464b681155fe28e95d94c44061",
+    "url": "/my-app/static/media/where-img02.b02b1c46.png"
+  },
+  {
+    "revision": "9ae0f696286987125c80577cb3dd1308",
+    "url": "/my-app/static/media/where-img03.9ae0f696.png"
+  },
+  {
+    "revision": "0e15a43adf1ae81ac1ac6e45ebaafd23",
+    "url": "/my-app/static/media/ticketswap-img00.0e15a43a.png"
+  },
+  {
+    "revision": "816c0fa19107854a9d87dd3c4abb82c4",
+    "url": "/my-app/static/media/ticketswap-img01.816c0fa1.png"
+  },
+  {
+    "revision": "d1bc6ec84cec70eff6f765e31eef9182",
+    "url": "/my-app/static/media/ticketswap-img02.d1bc6ec8.png"
+  },
+  {
+    "revision": "fcea283f1f208a2d3843b3a84d9746f7",
+    "url": "/my-app/static/media/studentass-img00.fcea283f.png"
+  },
+  {
+    "revision": "b06038b135c076a4b1702e066684e0f9",
+    "url": "/my-app/static/media/studentass-img01.b06038b1.png"
+  },
+  {
+    "revision": "c4211b2bd92bdc082bfb816918644294",
+    "url": "/my-app/static/media/studentass-img02.c4211b2b.png"
+  },
+  {
+    "revision": "aa6610a8579554b23a1d86609f87dafb",
+    "url": "/my-app/static/media/studentass-img03.aa6610a8.gif"
+  },
+  {
+    "revision": "5a445e1a7295232b1795921aadc84d17",
+    "url": "/my-app/static/media/studentass-img04.5a445e1a.gif"
+  },
+  {
+    "revision": "5eebc72fa6a45e23fab3c6332ca5c7e8",
+    "url": "/my-app/static/media/studentass-img05.5eebc72f.png"
+  },
+  {
+    "revision": "a5a2f697699699fa7569464eb9f9a977",
+    "url": "/my-app/static/media/memory-img00.a5a2f697.png"
+  },
+  {
+    "revision": "7dd1dd9faa52f8254d70ae15c09034c3",
+    "url": "/my-app/static/media/memory-img01.7dd1dd9f.png"
+  },
+  {
+    "revision": "6abd67b1af4c15337b266c98fe6adbac",
+    "url": "/my-app/static/media/memory-img02.6abd67b1.png"
+  },
+  {
+    "revision": "6bad51fa78ab4d0db33c33da8df88437",
+    "url": "/my-app/static/media/memory-img03.6bad51fa.png"
+  },
+  {
+    "revision": "edb4702ba2221ec25ea9fac9cd5f20e7",
+    "url": "/my-app/static/media/article-tn-01.edb4702b.png"
+  },
+  {
+    "revision": "9e62ddf0d6a51d234db256f9cacc7791",
+    "url": "/my-app/static/media/article-tn-02.9e62ddf0.png"
+  },
+  {
+    "revision": "5708fd35ec06a3f0ed10caa82b664dd5",
+    "url": "/my-app/static/media/article-tn-03.5708fd35.png"
+  },
+  {
+    "revision": "c36266d3e023aab006b5f861c183ded0",
+    "url": "/my-app/static/media/article-tn-04.c36266d3.png"
+  },
+  {
+    "revision": "4b69bae1c51b147e207cb0aed0086b6c",
+    "url": "/my-app/static/media/article-tn-05.4b69bae1.jpeg"
+  },
+  {
+    "revision": "1623581812d09ed5a172c92d4de2d249",
+    "url": "/my-app/static/media/article-tn-06.16235818.png"
+  },
+  {
+    "revision": "13ebe92ff66db4070bb27ae41d040009",
+    "url": "/my-app/static/media/profile.13ebe92f.png"
+  },
+  {
+    "revision": "a8e5a1c868ae9ac706ddca90b716a031",
+    "url": "/my-app/static/media/Veronica_Hvaal_Stigen_CV.a8e5a1c8.pdf"
+  },
+  {
+    "revision": "9dd4203f5af96a07b048f63b39e8fe35",
+    "url": "/my-app/static/media/link-icon.9dd4203f.svg"
+  },
+  {
+    "revision": "0c768863b084475e3760cf618d154919",
+    "url": "/my-app/static/media/github-logo.0c768863.png"
+  },
+  {
+    "revision": "82051cd055480ddd4ca5f684b641a37c",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.05079939710d95d62edaa02e4a1bf79e.js"
+  "/my-app/precache-manifest.c9a7a1d8bccd88e85b283945fa7676a2.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute("/my-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
